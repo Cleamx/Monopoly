0ce1d0836199967383383696acf3a3f2b99524b6
clem <clementine.mirande@gmail.com>
2024-03-11 22:13:05 +0100

    .

--- demo/src/main/java/tp/monopoly/Cases.java
+++ demo/src/main/java/tp/monopoly/Cases.java
@@ -4,17 +4,32 @@ public class Cases {
     int cout;
     int id;
     String groupe;
+    Joueurs proprietaire;
 
     public Cases(int id, String nom, int cout, String groupe){
         this.id = id;
         this.nom = nom;
         this.cout = cout;  
         this.groupe = groupe;
+        this.proprietaire = null;
     }
 
     public int getPosition() {
         return this.id;
     }
+    
+    public Joueurs getProprietaire() {
+        return proprietaire;
+    }
+    
+    public void setProprietaire(Joueurs proprietaire) {
+        this.proprietaire = proprietaire;
+    }
+    
+    public boolean estAchetable() {
+        return proprietaire == null;
+    }
+
 
     @Override
     public String toString() {
