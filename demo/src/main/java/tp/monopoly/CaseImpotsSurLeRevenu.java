345e4b5d38f3e178ea15454d57f961d981a17f2f
Matthieu Michelozzi <michelozzimatthieu@gmail.com>
2024-04-04 10:55:17 +0200

    modification interface textuelle et conditions de jeu

--- demo/src/main/java/tp/monopoly/CaseImpotsSurLeRevenu.java
+++ demo/src/main/java/tp/monopoly/CaseImpotsSurLeRevenu.java
@@ -1,8 +1,8 @@
 package tp.monopoly;
 
 public class CaseImpotsSurLeRevenu extends CaseSpeciale{
-    public CaseImpotsSurLeRevenu(int id, String nom, int cout, String groupe){
-        super(id, nom, cout, groupe);
+    public CaseImpotsSurLeRevenu(int id, String nom, int cout, String groupe, int[] loyer, int coutMaison, int coutHotel){
+        super(id, nom, cout, groupe , loyer, coutMaison, coutHotel);
     }
 
     @Override
--- demo/src/main/java/tp/monopoly/CaseSpeciale.java
+++ demo/src/main/java/tp/monopoly/CaseSpeciale.java
@@ -1,8 +1,8 @@
 package tp.monopoly;
 
 public abstract class CaseSpeciale extends Cases{
-    public CaseSpeciale(int id, String nom, int cout, String groupe){
-        super(id, nom, cout, groupe);
+    public CaseSpeciale(int id, String nom, int cout, String groupe, int[] loyer, int coutMaison, int coutHotel){
+        super(id, nom, cout, groupe, new int[0], 0, 0);
     }
     public abstract void appliquerEffet(Joueurs joueurs);
     
